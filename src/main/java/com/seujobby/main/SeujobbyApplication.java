193d16e5a5244bc1faeb917d4414c9072beaee68
Fábio Penha <fabiopenha95@outlook.com>
2023-07-11 17:45:40 -0300

    first commit

---
+++ src/main/java/com/seujobby/main/SeujobbyApplication.java
@@ -0,0 +1,13 @@
+package com.seujobby.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeujobbyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SeujobbyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/seujobby/main/SeujobbyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.seujobby.main;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeujobbyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
