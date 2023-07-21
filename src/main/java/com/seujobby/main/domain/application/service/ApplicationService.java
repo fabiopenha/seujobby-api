9f96369f84bd99f43e9576153d8493073f951339
Fábio Penha <fabiopenha95@outlook.com>
2023-07-21 14:24:36 -0300

    services created

---
+++ src/main/java/com/seujobby/main/domain/application/service/ApplicationService.java
@@ -0,0 +1,27 @@
+package com.seujobby.main.domain.application.service;
+
+import org.springframework.stereotype.Service;
+
+
+@Service
+public class ApplicationService {
+    public void create(){
+
+    }
+
+    public void getOne(){
+
+    }
+
+    public void getAll(){
+
+    }
+
+    public void update(){
+
+    }
+
+    public void delete(){
+
+    }
+}
---
+++ src/main/java/com/seujobby/main/domain/company/service/CompanyService.java
@@ -0,0 +1,26 @@
+package com.seujobby.main.domain.company.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class CompanyService {
+    public void create(){
+
+    }
+
+    public void getOne(){
+
+    }
+
+    public void getAll(){
+
+    }
+
+    public void update(){
+
+    }
+
+    public void delete(){
+
+    }
+}
